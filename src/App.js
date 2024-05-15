01ba4779841b3af3326647cbf30faab4307a2fae
Marilu Chacon <156120860+lilbxbori@users.noreply.github.com>
2024-05-15 09:48:46 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       Weather App
-      
+     <footer>
+      This project was coded by{" "} <a href="https://responsive-portfolio-marilu.netlify.app/" target="_blank" rel="noreferrer">Marilu Chacon</a> and is  
+      <a href="https://github.com/lilbxbori/weather-react-app-final" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
     
-    
+    </footer> 
     </div>
   );
 }
