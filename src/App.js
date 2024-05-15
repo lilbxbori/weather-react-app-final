297da936ec3c376011c2ced6c875f43c97df856e
Marilu Chacon <156120860+lilbxbori@users.noreply.github.com>
2024-05-15 12:21:17 -0400

    Added html

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
@@ -6,11 +7,11 @@ function App() {
 
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
-      <button className="btn btn-primary">Hello</button>
+    
+      <Weather />
      <footer>
       This project was coded by{" "} <a href="https://responsive-portfolio-marilu.netlify.app/" target="_blank" rel="noreferrer">Marilu Chacon</a> and is  
-      <a href="https://github.com/lilbxbori/weather-react-app-final" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+      <a href="https://github.com/lilbxbori/weather-react-app-final" target="_blank" rel="noreferrer"> open-sourced on GitHub </a> {" "} and {" "} <a href="https://weather-react-app-final-mchacon.netlify.app/" target="_blank" rel="noreferrer"> hosted on Netlify </a>
     
     </footer> </div>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Weather.css"
+
+export default function Weather() {
+return (
+ <div className="Weather">
+  <form>
+    <div className="row"> 
+    <div className="col-9">
+    <input type="search" placeholder="Enter a City ..." className="form-control"></input></div>
+    <div className="col-3">
+    <input type="submit" value="Search" className="btn btn-primary"></input></div></div>
+  </form>
+    <h1>New York</h1>
+    <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+    </ul>
+<div className="row">
+<div className="col-6">
+<img src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png" alt="Mostly Cloudy"/>
+6°C</div>
+<div className="col-6">
+  <ul>
+    <li>Precipitation: 53%</li>
+    <li>Humidity: 87%</li>
+    <li>Wind: 14 km/h</li>
+  </ul>
+
+
+</div>
+</div>
+
+
+ </div>
+);
+
+
+}
