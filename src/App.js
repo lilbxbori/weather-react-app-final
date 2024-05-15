5e338b4ade3c161581112c26d9a2e1481cf8900f
Marilu Chacon <156120860+lilbxbori@users.noreply.github.com>
2024-05-15 11:28:27 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-      Weather App
+      <div className="container">
+      <h1>Weather App</h1>
+      <button className="btn btn-primary">Hello</button>
      <footer>
       This project was coded by{" "} <a href="https://responsive-portfolio-marilu.netlify.app/" target="_blank" rel="noreferrer">Marilu Chacon</a> and is  
       <a href="https://github.com/lilbxbori/weather-react-app-final" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
     
-    </footer> 
+    </footer> </div>
     </div>
   );
 }
