62da7327358267d1aa9cb37f28db3dd7a75655bf
Marilu Chacon <156120860+lilbxbori@users.noreply.github.com>
2024-05-15 19:52:06 -0400

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -8,12 +8,16 @@ function App() {
     <div className="App">
       <div className="container">
     
-      <Weather />
-     <footer>
-      This project was coded by{" "} <a href="https://responsive-portfolio-marilu.netlify.app/" target="_blank" rel="noreferrer">Marilu Chacon</a> and is  
-      <a href="https://github.com/lilbxbori/weather-react-app-final" target="_blank" rel="noreferrer"> open-sourced on GitHub </a> {" "} and {" "} <a href="https://weather-react-app-final-mchacon.netlify.app/" target="_blank" rel="noreferrer"> hosted on Netlify </a>
+        <Weather />
+        <footer>
+            This project was coded by{" "} <a href="https://responsive-portfolio-marilu.netlify.app/" target="_blank" rel="noreferrer">Marilu Chacon</a> 
+            {" "}  and is {" "} 
+            <a href="https://github.com/lilbxbori/weather-react-app-final" target="_blank" rel="noreferrer"> open-sourced on GitHub </a> 
+            {" "} and {" "}  
+            <a href="https://weather-react-app-final-mchacon.netlify.app/" target="_blank" rel="noreferrer"> hosted on Netlify </a>
+        </footer> 
     
-    </footer> </div>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -6,33 +6,47 @@ return (
  <div className="Weather">
   <form>
     <div className="row"> 
-    <div className="col-9">
-    <input type="search" placeholder="Enter a City ..." className="form-control"></input></div>
-    <div className="col-3">
-    <input type="submit" value="Search" className="btn btn-primary"></input></div></div>
-  </form>
+        <div className="col-9">
+           <input type="search" 
+           placeholder="Enter a City ..." 
+           className="form-control"
+            autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" 
+            value="Search" 
+            className="btn btn-primary w-100"
+            />
+          </div>
+      </div>
+     </form>
     <h1>New York</h1>
-    <ul>
+        <ul>
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
-    </ul>
-<div className="row">
-<div className="col-6">
-<img src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png" alt="Mostly Cloudy"/>
-6°C</div>
-<div className="col-6">
-  <ul>
-    <li>Precipitation: 53%</li>
-    <li>Humidity: 87%</li>
-    <li>Wind: 14 km/h</li>
-  </ul>
-
-
-</div>
-</div>
-
-
- </div>
+        </ul>
+          <div className="row mt-3">
+             <div className="col-6">
+                <div className="clearfix">
+                  <img 
+                  src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png" 
+                  alt="Mostly Cloudy"
+                  className="float-left"
+                  />
+                       <span className="temperature">6</span>
+                       <span className="unit">°C</span>
+                 </div>
+              </div>
+                <div className="col-6">
+                   <ul>
+                     <li>Precipitation: 53%</li>
+                     <li>Humidity: 87%</li>
+                    <li>Wind: 14 km/h</li>
+                   </ul>
+                </div>
+            </div>
+         </div>
 );
 
 
